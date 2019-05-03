9518cba24e629fab4f1a5f8edebc62b22c6c1798
RRaa <35782666+RRaa@users.noreply.github.com>
2019-05-03 22:41:59 +0300

    JavaFX projects. Close program.

---
+++ src/sample/ClosingProgramProperly.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ClosingProgramProperly extends Application {
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window =primaryStage;
+        window.setTitle("Close program properly");
+
+        window.setOnCloseRequest(e->{
+            e.consume();
+            closeProgam();
+        });
+
+        button=new Button("Close program");
+        button.setOnAction( e-> closeProgam());
+
+        StackPane layout=new StackPane();
+        layout.getChildren().addAll(button);
+
+        Scene scene=new Scene(layout,300,300);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+    private void closeProgam(){
+        //System.out.println("File is saved!");
+        //window.close();
+
+        Boolean answer=ConfirmBox.display("Title","Are you sure, you want to exit?");
+        if (answer) window.close();
+    }
+}
