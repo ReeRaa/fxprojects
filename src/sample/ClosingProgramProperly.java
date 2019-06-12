c05480ab4a0548f12e5f9fddc40bc2dd529a8283
RRaa <35782666+RRaa@users.noreply.github.com>
2019-06-12 07:56:13 +0300

    JavaFX projects. Close program.

--- src/sample/ClosingProgramProperly.java
+++ src/sample/ClosingProgramProperly.java
@@ -13,7 +13,7 @@ public class ClosingProgramProperly extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+//
     @Override
     public void start(Stage primaryStage) {
         window =primaryStage;
